dfaccc873410f2cb6c89b9ed39653d35a79f4973
Kelsie Harris <kharris835@gmail.com>
2023-01-19 20:01:02 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Weather from "./Weather";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello from App!</h1>
+
+        <Weather />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Weather.js
@@ -0,0 +1,6 @@
+import React from "react";
+// import axios from "axios";
+
+export default function Weather() {
+  return <h2>Hello from Weather!</h2>;
+}
