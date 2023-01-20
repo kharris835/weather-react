c5df93a7d0aba548c51970c556f3d8a994746976
Kelsie Harris <kharris835@gmail.com>
2023-01-19 20:55:10 -0700

    Transfer formatting from non-react app version

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Weather from "./Weather";
 
@@ -6,9 +5,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from App!</h1>
-
         <Weather />
       </header>
     </div>
