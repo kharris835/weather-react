db727f366747751c981916032015bf75332f73dd
Kelsie Harris <kharris835@gmail.com>
2023-01-20 22:51:43 -0700

    style: current weather UI

--- src/Weather.js
+++ src/Weather.js
@@ -7,30 +7,34 @@ export default function Weather() {
       <div className="card">
         <div className="card-body">
           <form>
-            <div className="row">
+            <div className="row mt-2">
               <div className="col-7">
-                <input type="search" placeholder="Enter a city..." />
+                <input
+                  type="search"
+                  placeholder="Enter a city..."
+                  className="w-100"
+                />
               </div>
-              <div className="col-5">
+              <div className="col-5 text-center">
                 <input type="submit" value="Search" />
                 <input type="submit" value="Current Location" />
               </div>
             </div>
           </form>
-          <div className="row">
+          <div className="row align-items-center mt-4">
             <div className="col-4 align-self-center text-end">
               <img src="images/sun.svg" alt="Sunny" width="120px" />
             </div>
             <div className="col-4">
               <h2>45ºF</h2>
               <h1>Boulder</h1>
-              <ul>
+              <ul className="list-unstyled mb-0">
                 <li>Friday 6:09 PM</li>
                 <li>Sunny</li>
               </ul>
             </div>
             <div className="col-4">
-              <ul>
+              <ul className="p-0">
                 <li>Humidity: 57%</li>
                 <li>Precipitation: 0%</li>
                 <li>Wind: 1mph</li>
