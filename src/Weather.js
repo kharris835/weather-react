b19e5991eefe595a8c21094c1ef94f3a2e147cb9
Kelsie Harris <kharris835@gmail.com>
2023-01-22 20:55:17 -0700

    feat: import api

--- src/Weather.js
+++ src/Weather.js
@@ -1,19 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
 export default function Weather() {
+  const [input, setInput] = useState("");
+  const [weatherData, setweatherData] = useState(null);
+
+  function showCurrentWeather(response) {
+    console.log(response.data);
+    setweatherData({
+      temperature: response.data.main.temp,
+      description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    if (input.length === 0) {
+      return;
+    }
+    const apiKey = "b5b56bf4012bed80cd4ce11f2dda7ff2";
+    const units = "imperial";
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${apiKey}&units=${units}`;
+
+    axios.get(apiUrl).then(showCurrentWeather);
+  }
+
+  function updateInput(event) {
+    console.log(event.target.value);
+    setInput(event.target.value);
+  }
+  // console.log(weatherData);
+
   return (
     <div className="Weather">
       <div className="card">
         <div className="card-body">
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="row mt-2 mb-5">
               <div className="col-7">
                 <input
                   type="search"
                   placeholder="Enter a city..."
                   className="w-100 h-100"
-                  autoFocus="on"
+                  autoFocus={true}
+                  onChange={updateInput}
                 />
               </div>
               <div className="col-5 text-center">
