502aa89186d3b66bee6f32161b7de17d84d23d87
Kelsie Harris <kharris835@gmail.com>
2023-01-27 16:08:32 -0700

    feat: implement forecast day

--- src/Forecast.js
+++ src/Forecast.js
@@ -7,11 +7,20 @@ export default function Forecast({ data }) {
   const temperatureMax = Math.round(data.daily[0].temp.max);
   const temperatureMin = Math.round(data.daily[0].temp.min);
 
+  function day() {
+    let date = new Date(data.daily[0].dt * 1000);
+    let day = date.getDay();
+
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[day];
+  }
+
   return (
     <div className="Forecast text-center mt-4">
       <div className="row">
         <div className="col">
-          <div className="day pb-2">{data.daily[0].dt}</div>
+          <div className="day pb-2">{day()}</div>
           <WeatherIcon code={data.daily[0].weather[0].icon} size={42} />
           <div className="pt-1">
             <span className="temperature-max">{temperatureMax}º</span>
