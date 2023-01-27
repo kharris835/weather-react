ffefdb163a026a5d1c1e4a7c052a0c981f0c2ab8
Kelsie Harris <kharris835@gmail.com>
2023-01-27 16:41:33 -0700

    feat: 6 day forecast

--- src/Forecast.js
+++ src/Forecast.js
@@ -7,8 +7,8 @@ export default function Forecast({ data }) {
   const temperatureMax = Math.round(data.daily[0].temp.max);
   const temperatureMin = Math.round(data.daily[0].temp.min);
 
-  function day() {
-    let date = new Date(data.daily[0].dt * 1000);
+  function day(dailyForecast) {
+    let date = new Date(dailyForecast.dt * 1000);
     let day = date.getDay();
 
     let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
@@ -19,14 +19,18 @@ export default function Forecast({ data }) {
   return (
     <div className="Forecast text-center mt-4">
       <div className="row">
-        <div className="col">
-          <div className="day pb-2">{day()}</div>
-          <WeatherIcon code={data.daily[0].weather[0].icon} size={42} />
-          <div className="pt-1">
-            <span className="temperature-max">{temperatureMax}º</span>
-            <span className="temperature-min ms-2">{temperatureMin}º</span>
-          </div>
-        </div>
+        {data.daily.slice(0, 6).map(function (dailyForecast) {
+          return (
+            <div className="col">
+              <div className="day pb-2">{day(dailyForecast)}</div>
+              <WeatherIcon code={dailyForecast.weather[0].icon} size={42} />
+              <div className="pt-1">
+                <span className="temperature-max">{temperatureMax}º</span>
+                <span className="temperature-min ms-2">{temperatureMin}º</span>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
